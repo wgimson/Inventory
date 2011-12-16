8beaf86d7afd588a7bb7fcd77eebf26c71cac73c
William <wgimson@gmail.com>
2011-12-15 23:58:03 -0500

    first commit

---
+++ readline.h
@@ -0,0 +1,11 @@
+#ifndef READLINE_H
+#define READLINE_H 
+
+/*****************************************************************************
+ * read_line: Skips leading whitespace characters, then reads the remainder  *
+ *            of the input line and stores it in str. Truncates the line if  *
+ *            its length exceeds n. Returns the number of characters stored. *
+ *****************************************************************************/
+int read_line(char str[], int n);
+
+#endif
